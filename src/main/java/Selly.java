0ecca2bcc3c06b4f5ad1c1cce73ce1ae7c073f98
sscheol <sucheol@onycom.com>
2023-04-19 19:13:38 +0900

    <feat> Thread.sleep(3000)을 묵시적 wait으로 수정

--- src/main/java/Selly.java
+++ src/main/java/Selly.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 
 public class Selly {
@@ -28,19 +29,6 @@ public class Selly {
 
     }
 
-//    @Configuration
-//    public class PropertyConfig {
-//        @Bean(name = "system")
-//        public PropertiesFactoryBean propertiesFactoryBean() throws Exception {
-//            PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-//            ClassPathResource classPathResource = new ClassPathResource("com/application.yml");
-//
-//            propertiesFactoryBean.setLocation(classPathResource);
-//
-//            return propertiesFactoryBean;
-//        }
-//    }
-
 
     public Selly() {
 
@@ -49,13 +37,12 @@ public class Selly {
 //        option.addArguments("headless");
         driver = new ChromeDriver(option);
         driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://account.imqa.io/user/login");
 
     }
 
     public void run() throws Exception {
-
 
         WebElement idField = driver.findElement(By.cssSelector("html > body > div > div > div > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(2) > form > div:nth-of-type(1) > div:nth-of-type(1) > input"));
         idField.sendKeys("su10king@gmail.com");
@@ -68,6 +55,8 @@ public class Selly {
         WebElement loginButton = driver.findElement(By.cssSelector("button[class='submit']"));
         loginButton.click();
 
+//        WebDriverWait waitTimer = new WebDriverWait(driver, 6);
+//        waitTimer.until(ExpectedConditions.textToBePresentInElement(passwordField, "sucheol9608!"));
 
 //        Thread.sleep(4000);
 //
@@ -82,51 +71,45 @@ public class Selly {
 
         WebElement goMpm = driver.findElement(By.cssSelector("html > body > div:nth-of-type(1) > div > div:nth-of-type(2) > div:nth-of-type(2) > table > tbody > tr:nth-of-type(1) > td:nth-of-type(2) > div"));
         goMpm.click();
-        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-//        WebElement DropBox = driver.findElement(By.cssSelector("html > body > div > div > div:nth-of-type(1) > section > div > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(1) > div > button"));
-//        DropBox.click();
-//
-//        Thread.sleep(3000);
-//
-//        WebElement AppVersion = driver.findElement(By.cssSelector("html > body > div > div > div:nth-of-type(1) > section > div > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(1) > div > div > ul > li:nth-of-type(3) > button > span"));
-//        AppVersion.click();
-//
-//        Thread.sleep(3000);
 
-        System.out.println("MPM board");
 
 
         WebElement SummaryButton = driver.findElement(By.cssSelector("span[class$='center']"));
         SummaryButton.click();
-        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
         WebElement SummaryClose = driver.findElement(By.cssSelector("img[style='cursor: pointer; position: absolute; top: 0px; left: 600px;']"));
         SummaryClose.click();
-        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 //        알람 등록
         WebElement AlarmButton = driver.findElement(By.cssSelector("html > body > div > div > header > div:nth-of-type(2) > div > ul > div:nth-of-type(6) > li"));
         AlarmButton.click();
 
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement AlarmPolicy = driver.findElement(By.cssSelector("a[class='out']"));
         AlarmPolicy.click();
 
 
         WebElement AddAlarm = driver.findElement(By.cssSelector("button[class='add-btn']"));
         AddAlarm.click();
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         WebElement AlarmPolicyName = driver.findElement(By.cssSelector("input[class^='name-input']"));
         AlarmPolicyName.sendKeys("테스트 알람 자동 생성");
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         WebElement AlarmNativeRendering = driver.findElement(By.cssSelector("html > body > div > div > section > div > div:nth-of-type(2) > div > div:nth-of-type(2) > div:nth-of-type(3) > input"));
         AlarmNativeRendering.clear();
         AlarmNativeRendering.sendKeys("1");
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //경고알람을 메일로 받기 위해서 경고알림 수치를 낮춥니다.
 
         WebElement EmailSelect = driver.findElement(By.cssSelector("label[for='email-input']"));
@@ -135,27 +118,32 @@ public class Selly {
         AlarmMember.click();
         WebElement AlarmMemberSelect = driver.findElement(By.cssSelector("label[for='allSelect']"));
         AlarmMemberSelect.click();
-        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         WebElement AlarmSave = driver.findElement(By.cssSelector("button[class='submit']"));
         AlarmSave.click();
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //알람 수정
         WebElement ModifyAlarm = driver.findElement(By.cssSelector("html > body > div > div > section > div > div:nth-of-type(2) > table > tbody > tr > td:nth-of-type(2)"));
         ModifyAlarm.click();
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 
         WebElement ModifyAlarmPolicyName2 = driver.findElement(By.cssSelector("input[class='name-input']"));
         ModifyAlarmPolicyName2.clear();
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         ModifyAlarmPolicyName2.sendKeys("테스트 알람 자동 생성 후 수정됨");
         WebElement AlarmResponseTime = driver.findElement(By.cssSelector("html > body > div > div > section > div > div:nth-of-type(2) > div > div:nth-of-type(3) > div:nth-of-type(3) > input"));
         AlarmResponseTime.clear();
         AlarmResponseTime.sendKeys("1");
         WebElement ModifyAlarmSave = driver.findElement(By.cssSelector("button[class='submit']"));
         ModifyAlarmSave.click();
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
 
