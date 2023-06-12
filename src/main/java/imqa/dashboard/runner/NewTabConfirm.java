9caa5918ec8bfb8bea59b7b3b576557ac41d9392
sscheol <sucheol@onycom.com>
2023-06-12 17:15:01 +0900

    상세페이지로 탭 이동 및 데이터 조회 기능

---
+++ src/main/java/imqa/dashboard/runner/NewTabConfirm.java
@@ -0,0 +1,27 @@
+package imqa.dashboard.runner;
+
+import imqa.dashboard.dao.MetricDao;
+import imqa.dashboard.vo.ScenarioVo;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class NewTabConfirm implements SeleniumAction {
+
+    MetricDao dao;
+
+    NewTabConfirm(MetricDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public void run(WebDriver driver, ScenarioVo scenario) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement NewTabConfirm = driver.findElement(By.cssSelector(dao.findByElement(scenario).getCssSelect()));
+        String CurrentUrl = driver.getCurrentUrl();
+        driver.get(CurrentUrl);
+        NewTabConfirm.getText();
+    }
+}
